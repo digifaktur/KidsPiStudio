ad3845e2455156206f74aa6a90194dcf9c37c64c
Florian <florian.krisch@duatec.at>
2021-12-17 11:50:42 +0100

    Bild?

--- KidsPiStudio.py
+++ KidsPiStudio.py
@@ -5,7 +5,7 @@ class MainWindow(Frame):
         Frame.__init__(self, master)
         self.master = master
         self.pack(fill=BOTH, expand=1)
-        discButton = Button(self, command=self.clickDiscButton)
+        discButton = Button(self, image=PhotoImage("assets/cd2.png"), height=80, width=80, command=self.clickDiscButton)
         discButton.place(x=10, y=10)
     def clickDiscButton(self):
         exit()
