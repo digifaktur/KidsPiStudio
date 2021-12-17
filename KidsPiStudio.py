a4baf71166d512306f4d5a7f5300d6c43cf17eaa
Florian <florian@digifaktur.at>
2021-12-17 10:01:14 +0100

    Der Anfang

---
+++ KidsPiStudio.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+class MainWindow(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.pack(fill=BOTH, expand=1)
+        discButton = Button(self, command=self.clickDiscButton)
+        discButton.place(x=10, y=10)
+    def clickDiscButton(self):
+        exit()
+
+
+root = Tk()
+app = MainWindow(root)
+
+root.wm_title("KidsPiStudio")
+root.geometry("720x320")
+root.mainloop()
+
