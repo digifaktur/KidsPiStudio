9a7cfb3dd05d2270bee51ba26a90cc39f7b8e9a6
Florian <florian.krisch@duatec.at>
2021-12-17 12:12:21 +0100

    Bild geht

--- KidsPiStudio.py
+++ KidsPiStudio.py
@@ -5,7 +5,8 @@ class MainWindow(Frame):
         Frame.__init__(self, master)
         self.master = master
         self.pack(fill=BOTH, expand=1)
-        discButton = Button(self, image=PhotoImage("assets/cd2.png"), height=80, width=80, command=self.clickDiscButton)
+        self.cdimage=PhotoImage(file='./assets/cd2.png')
+        discButton = Button(self, image=self.cdimage, command=self.clickDiscButton)
         discButton.place(x=10, y=10)
     def clickDiscButton(self):
         exit()
@@ -13,7 +14,6 @@ class MainWindow(Frame):
 
 root = Tk()
 app = MainWindow(root)
-
 root.wm_title("KidsPiStudio")
 root.geometry("720x320")
 root.mainloop()
