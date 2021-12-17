99829441f3c12b99634b299bd2c9a007e0a6ed0b
Florian <florian.krisch@duatec.at>
2021-12-17 12:39:14 +0100

    Die ersten Knöpfe

--- KidsPiStudio.py
+++ KidsPiStudio.py
@@ -6,9 +6,24 @@ class MainWindow(Frame):
         self.master = master
         self.pack(fill=BOTH, expand=1)
         self.cdimage=PhotoImage(file='./assets/cd2.png')
-        discButton = Button(self, image=self.cdimage, command=self.clickDiscButton)
+        self.notesimage=PhotoImage(file='./assets/notes.png')
+        self.recordimage=PhotoImage(file='./assets/record.png')
+        self.exitimage=PhotoImage(file='./assets/exit.png')
+        discButton = Button(self, image=self.cdimage, width=160, command=self.clickDiscButton)
         discButton.place(x=10, y=10)
+        notesButton = Button(self, image=self.notesimage, width=160, command=self.clickNotesButton)
+        notesButton.place(x=10, y=160)
+        recordButton = Button(self, image=self.recordimage, width=160, command=self.clickRecordButton)
+        recordButton.place(x=180, y=10)
+        exitButton = Button(self, image=self.exitimage, width=160, command=self.clickExitButton)
+        exitButton.place(x=520, y=160)
     def clickDiscButton(self):
+        exit()
+    def clickNotesButton(self):
+        exit()
+    def clickRecordButton(self):
+        exit()
+    def clickExitButton(self):
         exit()
 
 
