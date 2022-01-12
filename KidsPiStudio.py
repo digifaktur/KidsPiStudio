199ce0404bf152fbe3659280c6471e85ca106cf2
Florian <florian.krisch@duatec.at>
2022-01-12 14:11:31 +0100

    MediaPlayer into its own class. Some sanity concerning audacious.

--- KidsPiStudio.py
+++ KidsPiStudio.py
@@ -3,17 +3,14 @@ from tkinter import *
 from subprocess import Popen, PIPE
 from functools import partial
 
+from MediaPlayer import MediaPlayer
+
 class MainWindow(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
         self.pack(fill=BOTH, expand=1)
         self.currentWindow = master
-        self.playimage = PhotoImage(file='./assets/play.png')
-        self.pauseimage = PhotoImage(file='./assets/pause.png')
-        self.stopimage = PhotoImage(file='./assets/stop.png')
-        self.previmage = PhotoImage(file='./assets/prev.png')
-        self.nextimage = PhotoImage(file='./assets/next.png')
         self.cdimage=PhotoImage(file='./assets/cd2.png')
         self.playcdimage=PhotoImage(file='./assets/playCd.png')
         self.ripcdimage=PhotoImage(file='./assets/ripCd.png')
@@ -21,6 +18,7 @@ class MainWindow(Frame):
         self.recordimage=PhotoImage(file='./assets/record.png')
         self.exitimage=PhotoImage(file='./assets/exit.png')
         self.closeimage=PhotoImage(file='./assets/close.png')
+        self.mediaPlayer = None
         discButton = Button(self, image=self.cdimage, width=160, command=self.clickDiscButton)
         discButton.place(x=10, y=10)
         notesButton = Button(self, image=self.notesimage, width=160, command=self.clickNotesButton)
@@ -39,26 +37,10 @@ class MainWindow(Frame):
         ripDiscButton.place(x=10, y=160)
         closeButton = Button(discWindow, image=self.closeimage, width=160, command=discWindow.destroy)
         closeButton.place(x=520, y=160)
-    def clickPlayMediaButton(self):
-        playMediaWindow = Toplevel(self)
-        playMediaWindow.geometry("720x320")
-        playMediaWindow.title("KidsPiStudio - Media Player")
-        self.currentWindow = playMediaWindow
-        mediaPlayButton = Button(playMediaWindow, image=self.playimage, width=160, command=subprocess.call(['audtool', '--playback-play']))
-        mediaPlayButton.place(x=10, y=10)
-        mediaPauseButton = Button(playMediaWindow, image=self.pauseimage, width=160, command=subprocess.call(['audtool', '--playback-pause']))
-        mediaPauseButton.place(x=180, y=10)
-        mediaStopButton = Button(playMediaWindow, image=self.stopimage, width=160, command=subprocess.call(['audtool', '--playback-stop']))
-        mediaStopButton.place(x=350, y=10)
-        mediaPrevButton = Button(playMediaWindow, image=self.previmage, width=160, command=subprocess.call(['audtool', '--playlist-advance']))
-        mediaPrevButton.place(x=10, y=160)
-        mediaNextButton = Button(playMediaWindow, image=self.nextimage, width=160, command=subprocess.call(['audtool', '--playlist-reverse']))
-        mediaNextButton.place(x=180, y=160)
-        closeButton = Button(playMediaWindow, image=self.closeimage, width=160, command=self.closeWindow)
-        closeButton.place(x=520, y=160)
-    def closeWindow(self):
-        subprocess.call(['audtool', '--shutdown'])
-        self.currentWindow.destroy()
+    def clickPlayMediaButton(self, path):
+        if isinstance(self.mediaPlayer, MediaPlayer):
+            self.mediaPlayer.closeWindow(self.mediaPlayer)
+        self.mediaPlayer = MediaPlayer(self, path)
     def clickRipDiscButton(self):
         return
     def clickNotesButton(self):
---
+++ MediaPlayer.py
@@ -0,0 +1,36 @@
+import subprocess
+import time
+from tkinter import *
+
+
+class MediaPlayer(Frame):
+    def __init__(self, master=None, path=None):
+        subprocess.call(['killall', 'audacious'])
+        playMediaWindow = Toplevel(master)
+        playMediaWindow.geometry("720x320")
+        playMediaWindow.title("KidsPiStudio - Media Player")
+        playMediaWindow.protocol("WM_DELETE_WINDOW", self.closeWindow)
+        self.currentWindow = playMediaWindow
+        self.playimage = PhotoImage(file='./assets/play.png')
+        self.pauseimage = PhotoImage(file='./assets/pause.png')
+        self.stopimage = PhotoImage(file='./assets/stop.png')
+        self.previmage = PhotoImage(file='./assets/prev.png')
+        self.nextimage = PhotoImage(file='./assets/next.png')
+        subprocess.Popen(['audacious', '-H', path], creationflags=subprocess.DETACHED_PROCESS, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        #time.sleep(3)
+        #subprocess.call(['audtool', '--playlist-addurl', path])
+        mediaPlayButton = Button(playMediaWindow, image=self.playimage, width=160, command=subprocess.call(['audtool', '--playback-play']))
+        mediaPlayButton.place(x=10, y=10)
+        mediaPauseButton = Button(playMediaWindow, image=self.pauseimage, width=160, command=subprocess.call(['audtool', '--playback-pause']))
+        mediaPauseButton.place(x=180, y=10)
+        mediaStopButton = Button(playMediaWindow, image=self.stopimage, width=160, command=subprocess.call(['audtool', '--playback-stop']))
+        mediaStopButton.place(x=350, y=10)
+        mediaPrevButton = Button(playMediaWindow, image=self.previmage, width=160, command=subprocess.call(['audtool', '--playlist-advance']))
+        mediaPrevButton.place(x=10, y=160)
+        mediaNextButton = Button(playMediaWindow, image=self.nextimage, width=160, command=subprocess.call(['audtool', '--playlist-reverse']))
+        mediaNextButton.place(x=180, y=160)
+        closeButton = Button(playMediaWindow, image=self.closeimage, width=160, command=self.closeWindow)
+        closeButton.place(x=520, y=160)
+    def closeWindow(self):
+        subprocess.call(['audtool', '--shutdown'])
+        self.currentWindow.destroy()
